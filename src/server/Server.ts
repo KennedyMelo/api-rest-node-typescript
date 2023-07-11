f454d3a50e3a5702bf0a19d1643338f73e4cb92b
KennedyMelo <kennedycmelo@gmail.com>
2023-07-11 13:34:28 -0300

    feat: add yup translations and aborty early on validate method

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
-import {router} from './routes';
 
+import './shared/services/translationsYup';
+import {router} from './routes';
 const server = express();
 
 server.use(express.json());
