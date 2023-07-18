80a54b4a0f526dddd5eceb121d11010dc199e3a2
KennedyMelo <kennedycmelo@gmail.com>
2023-07-18 18:25:15 -0300

    feat: add cidades model and update cidade controller methods

--- src/server/controllers/cidades/UpdateById.ts
+++ src/server/controllers/cidades/UpdateById.ts
@@ -2,14 +2,14 @@ import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware';
 import { StatusCodes } from 'http-status-codes';
+import { ICidade } from '../../database/models';
 
 interface IParamProps {
   id?: Number;
 }
 
-interface IBodyProps {
-  nome: string;
-}
+interface IBodyProps  extends Omit<ICidade, 'id'> {}
+
 export const updateByIdValidation = validation((getSchema) => ({
   params: getSchema<IParamProps>(yup.object().shape({
     id: yup.number().integer().required().moreThan(0),
@@ -20,8 +20,6 @@ export const updateByIdValidation = validation((getSchema) => ({
 }));
 
 export const updateById = async (req: Request<IParamProps, {}, IBodyProps>, res: Response) => {
-  console.log(req.params);
-  console.log(req.body);
   if(Number(req.params.id) === 99999){
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       errors: {
