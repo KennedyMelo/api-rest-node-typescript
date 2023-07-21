5c5cffa46165ae9fba0554d63f660a3e07d6bcc3
KennedyMelo <kennedycmelo@gmail.com>
2023-07-21 19:28:32 -0300

    feat: add password cryptography in authentication

--- src/server/controllers/usuarios/SignIn.ts
+++ src/server/controllers/usuarios/SignIn.ts
@@ -4,6 +4,7 @@ import { validation } from '../../shared/middleware';
 import { StatusCodes } from 'http-status-codes';
 import { IUsuario } from '../../database/models';
 import { UsuariosProvider } from '../../database/providers/usuarios';
+import { PasswordCrypto } from '../../shared/services';
 
 interface IBodyProps extends Omit<IUsuario, 'id' | 'nome'> {}
 
@@ -19,13 +20,22 @@ export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
   const {email, senha} = req.body;
 
   const result = await UsuariosProvider.getByEmail(email);
-  if(result instanceof Error || senha !== result.senha){
+  if(result instanceof Error){
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
         default: 'Email ou senha são inválidos'
       }
     });
-  } 
-  
+  }
+
+  const passwordMatch = await PasswordCrypto.verifyPassword(senha, result.senha);
+  if(!passwordMatch){
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      errors: {
+        default: 'Email ou senha são inválidos'
+      }
+    });
+  }
+
   return res.status(StatusCodes.OK).json({accessToken: 'teste.teste.teste'});
 };
